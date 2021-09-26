f83a23a1c3da64354409fb814b17630ef9ca660a
Zakir.Kazimov <ramzan666666@gmail.com>
2021-09-26 17:04:21 +0300

    Initial Commit

---
+++ SwiftUI.HW4./SwiftUI_HW4_App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_HW4_App.swift
+//  SwiftUI.HW4.
+//
+//  Created by Zakir on 26.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_HW4_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
